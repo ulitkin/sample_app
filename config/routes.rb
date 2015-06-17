c8df852887318fd140479cde853798148de060cf
ulitkin <ulitkin@gmail.com>
2015-06-17 11:53:30 -0400

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
